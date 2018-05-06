55ad12c8ca54d74e94439cecaa46397e3f50560f
sailboatlarry@gmail.com <sailboatlarry@gmail.com>
2018-05-06 18:25:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,8 +9,7 @@ function theBeatlesPlay(musicians, instruments) {
 function johnLennonFacts(){
   var facts = ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice"];
   var newFacts = [];
-  newFacts.push("foo", "bar")
-  var i = 0
+    var i = 0
   while (i < facts.length) {
     newFacts.push(facts[i] + "!!!")
     i++;
